dafa8800498bce608be36a617d01113d10c44373
mateusz.bartkowiak <mateusz.bartkowiak@cognifide.com>
2019-11-23 09:33:54 +0100

    Default view is full-screen now.

--- app/src/main/java/com/gulderbone/cookieclicker/MainActivity.kt
+++ app/src/main/java/com/gulderbone/cookieclicker/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.gulderbone.cookieclicker
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        this.supportActionBar?.hide()
+        this.window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_main)
     }
 }
