23c937c3c974bd4e7a6be72b8c5a99da77963c9a
GulderBone <mateuszbartkowiak00@gmail.com>
2019-12-01 18:28:38 +0100

    8 | Game is now a singleton responsible for the whole progress

--- app/src/main/java/com/gulderbone/cookieclicker/Game.kt
+++ app/src/main/java/com/gulderbone/cookieclicker/Game.kt
@@ -1,18 +1,55 @@
 package com.gulderbone.cookieclicker
 
+import android.os.Handler
+import android.os.Looper
 import android.view.WindowManager
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.gulderbone.cookieclicker.cookieproducers.CookieProducer
 
-class Game {
-    companion object {
-        var score = 0
+object Game {
+    var score = 0.0
+    var cpm = 0.0
+    var producers = mutableMapOf<CookieProducer, Int>()
 
-        fun switchToFullScreen(activity: AppCompatActivity) {
-            activity.supportActionBar?.hide()
-            activity.window.setFlags(
-                WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN
-            )
+    fun startCountingCookies(counter: TextView) {
+        val mainHandler = Handler(Looper.getMainLooper())
+
+        mainHandler.post(object : Runnable {
+            override fun run() {
+                score += (cpm / 20)
+                counter.text = score.toInt().toString()
+                mainHandler.postDelayed(this, 50)
+            }
+        })
+    }
+
+    fun switchToFullScreen(activity: AppCompatActivity) {
+        activity.supportActionBar?.hide()
+        activity.window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+    }
+
+    fun producerPurchased(cookieProducer: CookieProducer) {
+        addProducer(cookieProducer)
+        recalculateCpm()
+        println("Producer ${cookieProducer.name} purchased, current cpm: $cpm")
+    }
+
+    private fun addProducer(cookieProducer: CookieProducer) {
+        if (producers.containsKey(cookieProducer)) {
+            producers[cookieProducer] = producers[cookieProducer]!!.plus(1)
+        } else {
+            producers[cookieProducer] = 1
+        }
+    }
+
+    private fun recalculateCpm() {
+        cpm = 0.0
+        producers.forEach { producer ->
+            cpm += producer.key.cpm * producer.value
         }
     }
 }
