57fd31926a98ea57727afb3e413c99888528ce85
mateusz.bartkowiak <mateusz.bartkowiak@cognifide.com>
2019-12-21 11:40:19 +0100

    5 | File helper added

---
+++ app/src/main/java/com/gulderbone/cookieclicker/utilities/FileHelper.kt
@@ -0,0 +1,15 @@
+package com.gulderbone.cookieclicker.utilities
+
+import android.content.Context
+
+class FileHelper {
+    companion object {
+        fun getTextFromResources(context: Context, resourceId: Int): String {
+            return context.resources.openRawResource(resourceId).use {
+                it.bufferedReader().use {
+                    it.readText()
+                }
+            }
+        }
+    }
+}
